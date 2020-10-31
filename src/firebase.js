a6e2dc3f33a15028781f41514de0b9ff4646ac2f
Mukesh swamy <59552894+mukeshswamy@users.noreply.github.com>
2020-10-31 13:12:16 +0530

    firebase-added

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARQy0xB73QHz4laemV86IjjEau5esqYko",
+  authDomain: "tik-tok-clone-b2fce.firebaseapp.com",
+  databaseURL: "https://tik-tok-clone-b2fce.firebaseio.com",
+  projectId: "tik-tok-clone-b2fce",
+  storageBucket: "tik-tok-clone-b2fce.appspot.com",
+  messagingSenderId: "261602399626",
+  appId: "1:261602399626:web:ca20375e6777ad246be473",
+  measurementId: "G-JQWWPX7CNT"
+};
